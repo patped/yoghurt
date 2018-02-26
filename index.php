b66149c08520d9215128edc3ba34fe3c65764835
Patrick Pedersen <patrick@ossia.net>
2018-02-26 12:16:24 +0100

    Fjernet gammel kode som ikke ble brukt lenger

--- index.php
+++ index.php
@@ -10,14 +10,13 @@
 	<link rel="stylesheet" href="stilark.css" type="text/css">
 </head>
 <body>
-	<?php 
+	<?php
 		if (isset($_POST['Søk'])) {
 			$søkeord = $_POST['textbox'];
-			
+
 		}else {
 			$søkeord = ":)";
 		}
-		
 	?>
 	<div class="loginn">
 		<input type="text" name="" id="Brukernavn" style="width: 75px; height: 15px">
@@ -26,13 +25,13 @@
 		style="width: 75px; height: 15px">
 		<br>
 		<input type="submit" name="" value="logg inn" style=" width: 65px; height: 20px">
-		
+
 	</div>
 	<h1>Hvilken smiley har bedriften fått?</h1>
 
 	<h2>Sjekk det her</h2>
-	
-	
+
+
 	<input type="submit" alt="Skriv inn bedrift" name="text" id="søke" value="knapp">
 	<input type="submit" name="knapp" id="Søk" value="Knappeti">
 	<input type="submit" name="" value="knapp">
@@ -42,18 +41,6 @@
 	<input type="text" name="Søkefelt" value="Søkefelt">
 	<br>
 
-	<p>Skriv inn bedriften du ønsker å søke etter</p>
-
-	<table align="center" width="40%" border="1" cellspacing="1" cellpadding="0">
-	<!--
-	<?php
-	// Eksempel inndata
-	// ID, navn på bedrift, smilefjes/vurdering
-	
-	<input type="textbox" alt="Skriv inn bedrift">
-	<?php
-	// hent bedrifter
-	?>
 	</table>
 
 </body>
