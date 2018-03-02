1eb7a5a5c85f636d7c365fe9bac4e2954e0b4cec
Patrick Pedersen <patrick@ossia.net>
2018-03-02 18:11:14 +0100

    FIksa indenting og ryddet bort noen småfeil på siden.

--- index.php
+++ index.php
@@ -1,44 +1,43 @@
 <?php
-	include_once 'database.php';
-	$db = kobleOpp($tilsynrapportConfig);
+    require_once 'database.php';
+    $db = kobleOpp($tilsynrapportConfig);
 ?>
 
 <!doctype html>
 <html>
-<head>
-<meta charset="utf-8">
-<title>Forside youghurt</title>
-	<link rel="stylesheet" href="stilark.css" type="text/css">
-</head>
-<body>
-	<div class="loginn">
-		<input type="text" name="" id="Brukernavn" style="width: 75px; height: 15px">
-		<br>
-		<input type="passord" name="" id="passord"
-		style="width: 75px; height: 15px">
-		<br>
-		<input type="submit" name="" value="logg inn" style=" width: 65px; height: 20px">
+    <head>
+        <meta charset="utf-8">
+        <title>Forside youghurt</title>
+        <link rel="stylesheet" href="stilark.css" type="text/css">
+    </head>
+    <body>
+        <div class="loginn">
+            <input type="text" name="" id="Brukernavn" style="width: 75px; height: 15px">
+            <br>
+            <input type="passord" name="" id="passord"
+            style="width: 75px; height: 15px">
+            <br>
+            <input type="submit" name="" value="logg inn" style=" width: 65px; height: 20px">
+        </div>
 
-	</div>
-	<h1>Hvilken smiley har bedriften fått?</h1>
+        <h1>Hvilken smiley har bedriften fått?</h1>
 
-	<h2>Sjekk det her</h2>
+        <h2>Sjekk det her</h2>
 
-	<form action="sokeresultat.php" method="POST">
-	<label><input type="checkbox" alt="Skriv inn bedrift" name="text" id="søke" value="knapp">knapp</label>
-	<label><input type="checkbox" name="knapp" id="Søk" value="Knappeti">Knappeti</label>
-	<label><input type="checkbox" name="" value="knapp">knapp</label>
+        <form action="sokeresultat.php" method="POST">
+            <label><input type="checkbox" alt="Skriv inn bedrift" name="text" id="søke" value="knapp">knapp</label>
+            <label><input type="checkbox" name="knapp" id="Søk" value="Knappeti">Knappeti</label>
+            <label><input type="checkbox" name="" value="knapp">knapp</label>
 
-	<br><br>
+            <br><br>
 
-	<input type="text" name="Søkefelt" value="" placeholder="Søk på spisested">
-	<input type="submit" name="søkeKnapp" value="Utfør søk">
-	</form>
-	<br>
+            <input type="text" name="Søkefelt" value="" placeholder="Søk på spisested">
+            <input type="submit" name="søkeKnapp" value="Utfør søk">
+        </form>
 
-	</table>
+        <br>
 
-</body>
+    </body>
 </html>
 
 <?php
