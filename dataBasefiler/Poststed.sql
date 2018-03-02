39c7eb06edebbc0f2a882a15114ca2987dcbb9cd
Ola <olastaalberg@gmail.com>
2018-03-02 10:22:26 +0100

    Vet ikke hva som skjedde her

--- dataBasefiler/Poststed.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS Poststed;
-CREATE TABLE Poststed (
-	postnr CHAR(5),
-	poststed VARCHAR(50),
-	PRIMARY KEY (postnr)
-)Engine = INNODB;
-INSERT IGNORE INTO Poststed 
-SELECT DISTINCT mt.postnr, mt.poststed
-FROM matTabell as mt WHERE mt.poststed LIKE '%%'; -- Mister 13 dublikater. FUNKER IKKE UTEN IGNORE
--- dataBasefiler/kravPunkter.sql
+++ dataBasefiler/kravPunkter.sql
@@ -1,11 +1,9 @@
-DROP TABLE IF EXISTS Kravpunkter;
-create table Kravpunkter (
+DROP TABLE IF EXISTS kravpunkter;
+create table kravpunkter (
 	tilsynid varchar(50),
 	dato int,
 	ordingsverdi varchar(150),
 	kravpunktnavn_no varchar (160),
 	karakter tinyint,
-	tekst_no varchar(160),
-	PRIMARY KEY (tilsynid, ordingsverdi),
-	FOREIGN KEY (tilsynid) REFERENCES Tilsynsrapporter(tilsynid)
-)ENGINE = INNODB;
+	tekst_no varchar(160)
+);
