4a6f7379066fd072b694bd9b99890f91101561aa
Kim <kimandreness@gmail.com>
2018-03-02 12:10:53 +0100

    Fant filene dine ola! :)

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # yoghurt
 Skole prosjekt i databaser og web
-
-#  [wikisiden](https://github.com/patped/yoghurt/wiki) hvor man finner instruksjoner for å bruke git
--- dataBasefiler/kravPunkter.sql
+++ dataBasefiler/kravPunkter.sql
@@ -5,5 +5,7 @@ create table kravpunkter (
 	ordingsverdi varchar(150),
 	kravpunktnavn_no varchar (160),
 	karakter tinyint,
-	tekst_no varchar(160)
-);
+	tekst_no varchar(160),
+	PRIMARY KEY (tilsynid, ordingsverdi),
+	FOREIGN KEY (tilsynid) REFERENCES Tilsynsrapporter(tilsynid)
+)ENGINE = INNODB;
