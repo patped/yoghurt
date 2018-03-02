e0b8cb15e93b7633874ed2b471bce924726b4a81
Patrick Pedersen <patrick@ossia.net>
2018-03-02 15:54:41 +0100

    Importert jquery og bootstrap. Begynt på knappefunksjonaliteten

--- index.php
+++ index.php
@@ -4,47 +4,59 @@
 ?>
 
 <!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Forside youghurt</title>
-	<link rel="stylesheet" href="stilark.css" type="text/css">
-</head>
-<body>
-	<?php
-		if (isset($_POST['Søk'])) {
-			$søkeord = $_POST['textbox'];
+<html lang="no">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="bibloteker/bootstrap-4.0.0-dist/css/bootstrap.css">
+        <link rel="stylesheet" href="stilark.css" type="text/css">
 
-		}else {
-			$søkeord = ":)";
-		}
-	?>
-	<div class="loginn">
-		<input type="text" name="" id="Brukernavn" style="width: 75px; height: 15px">
-		<br>
-		<input type="passord" name="" id="passord"
-		style="width: 75px; height: 15px">
-		<br>
-		<input type="submit" name="" value="logg inn" style=" width: 65px; height: 20px">
+        <title>Forside youghurt</title>
 
-	</div>
-	<h1>Hvilken smiley har bedriften fått?</h1>
+    </head>
+    <body>
+        <?php
+        if (isset($_POST['Søk'])) {
+            $søkeord = $_POST['textbox'];
 
-	<h2>Sjekk det her</h2>
+        } else {
+            $søkeord = ":)";
+        }
+        ?>
+        <div class="loginn">
+            <input type="text" name="" id="Brukernavn" style="width: 75px; height: 15px">
+            <br>
+            <input type="passord" name="" id="passord"
+            style="width: 75px; height: 15px">
+            <br>
+            <input type="submit" name="" value="logg inn" style=" width: 65px; height: 20px">
+
+        </div>
+        <h1>Hvilken smiley har bedriften fått?</h1>
+
+        <h2>Sjekk det her</h2>
+
+        <br>
+
+        <div class="container-fluid">
+            <a data-role="button" name="navn" id="navn">Navn</a>
+            <a data-role="button" name="orgnummer" id="orgnummer">Org.nummer</a>
+            <a data-role="button" name="adrlinje" id="adrlinje">Adresse</a>
+            <a data-role="button" name="postnr" id="postnr">Post.nr</a>
+        </div>
+
+        <br>
+        <br>
+
+        <div class="container-floid">
+            <input type="text" name="Søkefelt" value="Søkefelt">
+        </div>
 
 
-	<input type="submit" alt="Skriv inn bedrift" name="text" id="søke" value="knapp">
-	<input type="submit" name="knapp" id="Søk" value="Knappeti">
-	<input type="submit" name="" value="knapp">
-
-	<br><br>
-
-	<input type="text" name="Søkefelt" value="Søkefelt">
-	<br>
-
-	</table>
-
-</body>
+        <script src="bibloteker/jquery/jquery-3.3.1.js"></script>
+        <script src="bibloteker/bootstrap-4.0.0-dist/js/bootstrap.bundle.js"></script>
+        <script src="js/index.js"></script>
+    </body>
 </html>
 
 <?php
