b754204e24652a860c32b0e3e455ebb1ee717852
Unknown <olastaalberg@gmail.com>
2018-03-06 18:21:29 +0100

    Lagt inn søkemuligheter. Har ikke fullført noe enda, mangler også spørringer og IF'er basert på valg i sokeresultat.

--- index.php
+++ index.php
@@ -24,24 +24,35 @@
 
         <h2>Sjekk det her</h2>
 
-        <form action="sokeresultat.php" method="POST">
+        <form action="sokeresultat.php" method="POST" onsubmit="return sjekkForm()">
             <label><input type="checkbox" onclick="orgKlikk()" name="orgnr" id="orgnr" value="">Søk på organisasjonsnummer</label>
             <label><input type="checkbox" onclick="adresseKlikk()" name="adresse" id="adresse" value="">Søk på adresse</label>
             <label><input type="checkbox" onclick="restaurantKlikk()" name="restaurant" id="restaurant" value="">Søk på spisested</label>
 
             <br><br>
-            <label hidden="false" id="hovedsøkLabel">Spisested: </label><input type="text" id="sokeFelt" name="Søkefelt" value="" placeholder="Søk på navnet til spisested" hidden="true">
+            <label hidden="true" id="spisestedLabel">Navn på spisested: </label><input type="text" id="spisestedSokefelt" name="spisestedSokefelt" value="" placeholder="Søk på navnet til spisested" hidden="true">
+            <br>
+            <label hidden="true" id="adresseLabel">Adresse: </label><input type="text" id="sokeFelt" name="Søkefelt" value="" placeholder="Søk på navnet til spisested" hidden="true">
             <br>
             <label hidden="true" id="poststedLabel">Poststed: <input type="text" id="poststedInput" name="poststedInput" value="" placeholder="Poststed" hidden="true"></label>
             <br>
-            <input type="submit" id="utforSok" name="søkeKnapp" value="Utfør søk">
+            <input type="submit" id="utforSok" name="søkeKnapp" value="Utfør søk" disabled="true">
         </form>
 
         <br>
 
         <script type="text/javascript">
+        	function sjekkForm(){
+        		var orgInnhold = document.getElementById("sokeFelt").value;
+        		if (document.getElementById("orgnr").checked && orgInnhold =="") {
+        			alert("Du må fylle inn organisasjonsnummer");
+        			return false;
+        		}
+        	}
 			function orgKlikk(){
 				if(document.getElementById("orgnr").checked) {
+					document.getElementById("sokeFelt").pattern = "[0-9]{9}";
+					document.getElementById("sokeFelt").title = "Et organisasjonsnummer består av 9 siffer"
 					document.getElementById("adresse").checked = false;
 					document.getElementById("restaurant").checked = false;
 					document.getElementById("sokeFelt").placeholder="Søk på orgnummer";
@@ -50,42 +61,66 @@
 					document.getElementById("poststedInput").hidden = true;
 					document.getElementById("adresse").disabled = true;
 					document.getElementById("restaurant").disabled = true;
+					document.getElementById("utforSok").disabled = false;
 				}
 				else{
 					document.getElementById("sokeFelt").placeholder="Søk på navnet til spisested";
+					document.getElementById("sokeFelt").value = "";
 					document.getElementById("restaurant").disabled = false;
 					document.getElementById("adresse").disabled = false;
 					document.getElementById("sokeFelt").hidden = true;
+					document.getElementById("utforSok").disabled = true;
+					document.getElementById("sokeFelt").removeAttribute("pattern");
+					document.getElementById("sokeFelt").removeAttribute("title");
+					
 
 				}
 			}
 			function adresseKlikk(){
 				if(document.getElementById("adresse").checked) {
+
 					document.getElementById("orgnr").checked = false;
 					document.getElementById("sokeFelt").placeholder="Søk på adresse";
 					document.getElementById("sokeFelt").hidden = false;
 					document.getElementById("poststedLabel").hidden = false;
+					document.getElementById("adresseLabel").hidden = false;
 					document.getElementById("poststedInput").hidden = false;
 					document.getElementById("orgnr").disabled = true;
+					document.getElementById("utforSok").disabled = false;
 				}else{
+					if (!document.getElementById("restaurant").checked) {
+						document.getElementById("orgnr").disabled = false;
+					}
 					document.getElementById("sokeFelt").placeholder="Søk på navnet til spisested";
-					document.getElementById("poststedLabel").hidden = false;
-					document.getElementById("poststedInput").hidden = false;
-					document.getElementById("orgnr").disabled = false;
+					document.getElementById("poststedLabel").hidden = true;
+					document.getElementById("poststedInput").hidden = true;
 					document.getElementById("sokeFelt").hidden = true;
+					document.getElementById("sokeFelt").value = "";
+					document.getElementById("adresseLabel").hidden = true;
+					if (!document.getElementById("restaurant").checked && !document.getElementById("orgnr").checked) {
+						document.getElementById("utforSok").disabled = true;
+					}
 				}
 			}
 			function restaurantKlikk(){
 				if(document.getElementById("restaurant").checked) {
 					document.getElementById("orgnr").checked = false;
-					document.getElementById("adresse").checked = false;
-					document.getElementById("sokeFelt").placeholder="Søk på navnet til spisested";
-					document.getElementById("poststedLabel").hidden = true;
-					document.getElementById("poststedInput").hidden = true;
+					document.getElementById("spisestedLabel").hidden = false;
+					document.getElementById("spisestedSokefelt").hidden = false;
+					document.getElementById("spisestedSokefelt").placeholder="Søk på navnet til spisested";
 					document.getElementById("orgnr").disabled = true;
+					document.getElementById("utforSok").disabled = false;
 				}
 				else{
-					document.getElementById("orgnr").disabled = false;
+					if (!document.getElementById("adresse").checked) {
+						document.getElementById("orgnr").disabled = false;
+					}
+					document.getElementById("spisestedLabel").hidden = true;
+					document.getElementById("spisestedSokefelt").hidden = true;
+					if (!document.getElementById("adresse").checked && !document.getElementById("orgnr").checked) {
+						document.getElementById("utforSok").disabled = true;
+					}
+					
 				}
 			}
         </script>
