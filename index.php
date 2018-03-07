25da41c640a6a5d7c7f7c17b9d70433c8d1b013e
Patrick Pedersen <patrick@ossia.net>
2018-03-07 12:48:11 +0100

    Marged index.html from master

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-	include_once 'database.php';
-	$db = kobleOpp($tilsynrapportConfig);
+    require_once 'database.php';
+    $db = kobleOpp($tilsynrapportConfig);
 ?>
 
 <!doctype html>
@@ -10,49 +10,33 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <link rel="stylesheet" href="bibloteker/bootstrap-4.0.0-dist/css/bootstrap.css">
         <link rel="stylesheet" href="stilark.css" type="text/css">
+    </head>
+    <body>
 
         <title>Forside youghurt</title>
 
-    </head>
-    <body>
-        <?php
-        if (isset($_POST['Søk'])) {
-            $søkeord = $_POST['textbox'];
-
-        } else {
-            $søkeord = ":)";
-        }
-        ?>
-        <div class="loginn">
-            <input type="text" name="" id="Brukernavn" style="width: 75px; height: 15px">
-            <br>
-            <input type="passord" name="" id="passord"
-            style="width: 75px; height: 15px">
-            <br>
-            <input type="submit" name="" value="logg inn" style=" width: 65px; height: 20px">
-
-        </div>
-        <h1>Hvilken smiley har bedriften fått?</h1>
-
+	<div class="loginn">
+		<form method="POST" action="Brukerside.php">
+		<input type="text" name="bruker" id="Brukernavn"  style="width: 75px; height: 15px">
+		<br>
+		<input type="password" name="passord" id="pass"
+		style="width: 75px; height: 15px">
+		<br>
+		<input type="submit" name="" value="logg inn" style=" width: 65px; height: 20px">
+		</form>
+    </div>
         <h2>Sjekk det her</h2>
 
-        <br>
+        <form action="sokeresultat.php" method="POST">
+            <label><input type="checkbox" alt="Skriv inn bedrift" name="text" id="søke" value="knapp">knapp</label>
+            <label><input type="checkbox" name="knapp" id="Søk" value="Knappeti">Knappeti</label>
+            <label><input type="checkbox" name="" value="knapp">knapp</label>
 
-        <div class="container-fluid">
-            <a data-role="button" name="navn" id="navn">Navn</a>
-            <a data-role="button" name="orgnummer" id="orgnummer">Org.nummer</a>
-            <a data-role="button" name="adrlinje" id="adrlinje">Adresse</a>
-            <a data-role="button" name="postnr" id="postnr">Post.nr</a>
-        </div>
+            <br><br>
 
-        <br>
-        <br>
-
-        <div class="container-floid">
-            <input type="text" name="Søkefelt" value="Søkefelt">
-        </div>
-
-
+            <input type="text" name="Søkefelt" value="" placeholder="Søk på spisested">
+            <input type="submit" name="søkeKnapp" value="Utfør søk">
+        </form>
         <script src="bibloteker/jquery/jquery-3.3.1.js"></script>
         <script src="bibloteker/bootstrap-4.0.0-dist/js/bootstrap.bundle.js"></script>
         <script src="js/index.js"></script>
