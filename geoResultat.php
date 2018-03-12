7baa989aee11e52c77cb0080f8ea4e6173568b63
Patrick Pedersen <patrick@ossia.net>
2018-03-12 20:32:24 +0100

    Fjernet testkode fra distanse()

--- geoResultat.php
+++ geoResultat.php
@@ -2,13 +2,9 @@
 function distanse($resturantLat, $resturantLong) {
     $userLat = $_POST['lat'];
     $userLong = $_POST['long'];
-    echo "lat $userLat <br>";
-    echo "long $userLong <br>";
     $x = 69.1 * ($resturantLat-$userLat);
-    echo "x = $x <br>";
     $y = 69.1 * ($resturantLong-$userLong) * cos(($userLat/57.3));
     $distanseMiles = sqrt(pow($x , 2) + pow($y , 2));
     $distanseKilometer = $distanseMiles*1.609344;
-    echo $distanseKilometer;
 }
 ?>
