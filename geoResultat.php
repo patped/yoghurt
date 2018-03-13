ca623fa05879c95143b6e3d110d34ca2fc940ad6
Patrick Pedersen <patrick@ossia.net>
2018-03-13 22:48:06 +0100

    modifisert geosøk til å fungere med index siden

--- geoResultat.php
+++ geoResultat.php
@@ -1,35 +1,28 @@
 <?php
-require_once 'database.php';
 
-$db = kobleOpp($tilsynrapportConfig);
-$status = mysqli_set_charset($db, "utf8");
-$sql = ("SELECT * FROM Restauranter WHERE latitude IS NOT NULL;");
-if ($status) {
+function iNaerheten($db, $userLat, $userLong) {
+    $resultat = [];
+    $sql = ("SELECT * FROM Restauranter WHERE latitude IS NOT NULL;");
     $result = mysqli_query($db, $sql);
 
     while ($rad = mysqli_fetch_assoc($result)) {
         $latitude = $rad['latitude'];
         $longitude = $rad['longitude'];
-        $distanse = distanse($latitude, $longitude);
+        $distanse = distanse($userLat, $userLong, $latitude, $longitude);
+        echo $distanse;
         if ($distanse < 5) {
-            $navn = $rad['navn'];
-            echo $navn." distanse: ".$distanse."<br>";
+            $resultat[] = $rad;
         }
     }
-} else {
-    echo "DB FEIL!";
-    exit;
+    return $resultat;
 }
 
-
-function distanse($resturantLat, $resturantLong) {
-    $userLat = $_POST['lat'];
-    $userLong = $_POST['long'];
+function distanse($userLat, $userLong, $resturantLat, $resturantLong) {
     $x = 69.1 * ($resturantLat-$userLat);
     $y = 69.1 * ($resturantLong-$userLong) * cos(($userLat/57.3));
     $distanseMiles = sqrt(pow($x , 2) + pow($y , 2));
     $distanseKilometer = $distanseMiles*1.609344;
     return $distanseKilometer;
 }
-lukk($db);
+
 ?>
