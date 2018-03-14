c74110ceee4291dd9d034ac7533674792ab239ff
Patrick Pedersen <patrick@ossia.net>
2018-03-14 15:50:28 +0100

    Endret bootstrap versjon til den samme som er på w3school

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="bibloteker/bootstrap-4.0.0-dist/css/bootstrap.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <link rel="stylesheet" href="stilark.css" type="text/css">
     </head>
     <body>
@@ -44,9 +44,9 @@
 
         <br>
 
-        <script src="bibloteker/jquery/jquery-3.3.1.js"></script>
         <script src="js/index.js"></script>
-        <script src="bibloteker/bootstrap-4.0.0-dist/js/bootstrap.bundle.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         
     </body>
 </html>
