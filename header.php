a84c2fc39c3a6a274d0fd0e1f5414de75b41e92e
Ingeborg Vik Anundsen <Ingi.vik@gmail.com>
2018-03-14 11:23:58 +0100

    /ryddeKode:

--- header.php
+++ header.php
@@ -1,19 +1,22 @@
 <link rel="stylesheet" type="text/css" href="header.css">
-
+<link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
 <script>
 	function goBack() {
 		window.history.back();
 	}
 </script>
 
-<div id="nav">
+<div id="container">
+	<div class="jumbotron">
 
-<div class="venstreKnapper">
-<button onClick="window.location='index.php'" class="homeButton">Home</button>
-<button onClick="goBack()">Tilbake</button>
-</div>
+		<div class="venstreKnapper">
+		<button onClick="window.location='index.php'" class="homeButton">Home</button>
+		<button onClick="goBack()" class="backButton">Tilbake</button>
+		</div>
 
-<img src="bilder/tempLogo.jpg" href="yogurt/index.php">
+		<img src="bilder/tempLogo.jpg" href="yogurt/index.php">
+
+	</div>
 
 </div>
 
