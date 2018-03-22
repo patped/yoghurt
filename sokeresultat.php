302c4cda68099acd98e7aa08481de9821041afdc
Patrick Pedersen <patrick@ossia.net>
2018-03-22 15:57:28 +0100

    Lagt til pekerklikker på søkeresultatet

--- sokeresultat.php
+++ sokeresultat.php
@@ -156,7 +156,7 @@ session_start();
                         }
                     /*Legger til alle resultater i en tabell*/
                     echo "<tbody>";
-                    echo    "<tr class='clickable-link' data-href='restaurantVisning.php?res=$id'>";
+                    echo    "<tr class='clickable-link' data-href='restaurantVisning.php?res=$id' style='cursor:pointer'>";
                     echo        "<td>$rNavn</td>";
                     echo        "<td>$rAdresse</td>";
                     echo        "<td>$rPostnr</td>";
