282fa02f7a9b3985882570518f1647196d7a8e01
Patrick Pedersen <patrick@ossia.net>
2018-03-23 13:16:29 +0100

    Lagt funksjonene til legg til / rediger tilsynsrapport siden inn i en egen fil.

--- tilsynsrapport/leggTilNyTilsynsrapport.php
+++ tilsynsrapport/leggTilNyTilsynsrapport.php
@@ -1,209 +1,134 @@
 <?php
 session_start();
-?>
- <?php
-	include_once '../database.php';
-	include_once '../hjelpefunksj.php';
-	include_once 'oppdaterTilsynsrapport.php';
-	$tilsynid = $_SESSION['tilsynid'];
-	$tilsynsrapport = false;
-	if ($tilsynid) {
-		$tilsynsrapport = tilsynsrapport($tilsynid);
-	}
-	// TODO
-	// SJEKKE OM VARIABLER FORTSATT FUNGERER MED TRUE FALSE
-	//
-	//
-	//
-	$tilsynsobjektid = $tilsynsrapport['tilsynsobjektid'];
-	$dato = $tilsynsrapport['dato'];
-	$dato = substr($dato,0,2).".".substr($dato,2,2).".".substr($dato,4,4);
-	$tilsynsbesoektype = $tilsynsrapport['tilsynsbesoektype'];
-	$status = $tilsynsrapport['status'];
+require_once '../database.php';
+require_once '../hjelpefunksj.php';
+require_once 'oppdaterTilsynsrapport.php';
+
+$tilsynid = $_SESSION['tilsynid'];
+$tilsynsrapport = false;
+if ($tilsynid) {
+    $tilsynsrapport = tilsynsrapport($tilsynid);
+}
+$tilsynsobjektid = $tilsynsrapport['tilsynsobjektid'];
+$dato = $tilsynsrapport['dato'];
+$dato = substr($dato,0,2).".".substr($dato,2,2).".".substr($dato,4,4);
+$tilsynsbesoektype = $tilsynsrapport['tilsynsbesoektype'];
+$status = $tilsynsrapport['status'];
 ?>
 
 <!doctype html>
 <html lang="no">
 <head>
-  <title>Yoghurt</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<title>Yoghurt</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
-	<?php
-	include_once '../header.php';
+<?php
+include_once '../header.php';
+starAlertInnlogg();
+$side = 'Location: /tilsynsrapport/leggTilNyTilsynsrapport.php';
+logginn($side);
+?>
 
-    starAlertInnlogg();
-    $side = 'Location: /tilsynsrapport/leggTilNyTilsynsrapport.php';
-	logginn($side);
-	
-	//henter kravpunktnavn og ordningsverdi fra kravpunkter
+<div class="container">
+<div class="jumbotron">
+<h1 class="text-center">Tilsynsrapport</h1>
+</div>
+</div>
 
-	function tilsynsbesoektype($tilsynsbesoektype) {
-		if($tilsynsbesoektype == 0) {
-			echo "<option value='0'>Ordinært</option>";
-		} else if ($tilsynsbesoektype == 1) {
-			echo "<option value='1'>oppfølgings -tilsyn</option>";
-		}
-	}
+<div class="container">
+<div class="page-header"> <h2>bedrift...</h2> </div>
+<div class="table-responsive">
 
-	function status($status) {
-		if ($status == 0) {
-			echo "<option value='0'>utestående avvik finnes</option>";
-		} else if ($status == 1) {
-			echo "<option value='1'>alle avvik lukket</option>";
-		}
-	}
+<form method="POST" action="registrerTilsynsraport.php">
+<div class="col-xs-4">
+<table class="table">
+<thead>
+<th></th>
+<th></th>
+</thead>
+<tbody>
+<tr>
+<td>TilsynsobjektID:</td>
+<td><input type="text" name="tilsynsobjektid" <?php if($tilsynsobjektid) echo "value='$tilsynsobjektid'"; ?>></td>
+</tr>
 
-	function kravpunkter($tilsynid){
-		$db = kobleOpp();
-		if($tilsynid){
-			$sqlspørring = ("SELECT * FROM `Kravpunkter` WHERE `tilsynid` like '$tilsynid';");
+<tr>
+<td>TilsynsID:</td>
+<td>
+<input type="text" name="tilsynid" <?php if($tilsynid) {echo "value='$tilsynid'";} ?>></td>
+</tr>
 
-		}
-		else{
-			$sqlspørring = ("SELECT DISTINCT ordingsverdi,kravpunktnavn_no
-							FROM Kravpunkter;");
-		}
-		
-		$svar = mysqli_query( $db, $sqlspørring );
-		lukk($db);
+<tr>
+<td>TilsynsBesøksType:</td>
+<td>
+<select name="tilsynsbesoektype">
+<?php tilsynsbesoektype($tilsynsbesoektype); ?>
+<option value="0">Ordinært</option>
+<option value="1">oppfølgings -tilsyn</option>
+</select>
+</td>
+</tr>
 
-		$rad = mysqli_fetch_assoc($svar);
-		while ($rad) {
-			$ordingsverdi = $rad['ordingsverdi'];
-			$kravpunktnavn_no = $rad['kravpunktnavn_no'];
-			$karakter;
-			$tekst_no;
-			if($tilsynid){
-				$karakter = $rad['karakter'];
-				$tekst_no = $rad['tekst_no'];
-			}
+<tr>
+<td>dato:</td>
+<td><input type="text" name="dato" pattern="[0-3]{1}[0-9]{1}[.]{1}[0-1]{1}[0-9]{1}[.]{1}[1-9]{4}" placeholder="dd.mm.åååå" <?php if($tilsynid) {echo "value='$dato'";} ?>></td>
+</tr>
+</tbody>
+</table>
+</div>	
+<table class="table">
+<thead>
+<th class="col-xs-1">#</th>
+<th class="col-xs-5">Kravpunkt</th>
+<th class="col-xs-1">Karakter</th>
+<th class="col-xs-5">Komentar</th>
+</thead>
+<tbody>
+<?php kravpunkter($tilsynid); ?>
+<tr>
+<td></td>
+<td></td>
+<td>Status:</td>    
+<td> 
+<select name="status">
+<?php status($status); ?>
+<option value="0">utestående avvik finnes</option>
+<option value="1">alle avvik lukket</option>
+</select>
 
-			$temaOrdingsverdi = substr($ordingsverdi,0,1).'_'.substr($ordingsverdi,2,3);
-			echo"
-			<tr>
-				<td>$ordingsverdi:</td>
-				<td>$kravpunktnavn_no:</td>
-				<td>
-					<select class='karakter' name='karakter$temaOrdingsverdi'>
-						<option value='' selected disabled hidden>$karakter</option>
-						<option value='0'>0</option>
-						<option value='1'>1</option>
-						<option value='2'>2</option>
-						<option value='3'>3</option>
-						<option value='4'>4</option>
-						<option value='5'>5</option>
-					</select>
-				</td>
-				<td><input type='text' name='beskrivelse$temaOrdingsverdi' style='width: 100%;' value = '$tekst_no'></td>
-			</tr>
-			";
-			$rad= mysqli_fetch_assoc($svar);
+<input type="submit" name="submit" class="pull-right" >	
+</td>
+</tr>
+</tbody>
+</table>
+</form>
+</div>
+</div>
 
-		}
-	}
-
-	?>
-
-	<div class="container">
-    	<div class="jumbotron">
-      		<h1 class="text-center">Tilsynsrapport</h1>
-    	</div>
-  	</div>
-
-  	<div class="container">
-    	<div class="page-header"> <h2>bedrift...</h2> </div>
-		<div class="table-responsive">
-			
-			<form method="POST" action="registrerTilsynsraport.php">
-				<div class="col-xs-4">
-					<table class="table">
-						<thead>
-							<th></th>
-							<th></th>
-						</thead>
-						<tbody>
-							<tr>
-				    			<td>TilsynsobjektID:</td>
-				    			<td><input type="text" name="tilsynsobjektid" <?php if($tilsynsobjektid) echo "value='$tilsynsobjektid'"; ?>></td>
-				  			</tr>
-
-				  			<tr>
-				    			<td>TilsynsID:</td>
-				    			<td>
-								<input type="text" name="tilsynid" <?php if($tilsynid) {echo "value='$tilsynid'";} ?>></td>
-				  			</tr>
-
-							<tr>
-				    			<td>TilsynsBesøksType:</td>
-				    			<td>
-					    			<select name="tilsynsbesoektype">
-										<?php tilsynsbesoektype($tilsynsbesoektype); ?>
-							    		<option value="0">Ordinært</option>
-							    		<option value="1">oppfølgings -tilsyn</option>
-					  				</select>
-				  				</td>
-				  			</tr>
-
-							<tr>
-				    			<td>dato:</td>
-				    			<td><input type="text" name="dato" pattern="[0-3]{1}[0-9]{1}[.]{1}[0-1]{1}[0-9]{1}[.]{1}[1-9]{4}" placeholder="dd.mm.åååå" <?php if($tilsynid) {echo "value='$dato'";} ?>></td>
-				  			</tr>
-			  			</tbody>
-			  		</table>
-		  		</div>	
-		  		<table class="table">
-		  				<thead>
-		  					<th class="col-xs-1">#</th>
-		  					<th class="col-xs-5">Kravpunkt</th>
-		  					<th class="col-xs-1">Karakter</th>
-		  					<th class="col-xs-5">Komentar</th>
-		  				</thead>
-		  				<tbody>
-							<?php kravpunkter($tilsynid); ?>
-							<tr>
-				    			<td></td>
-				    			<td></td>
-				  				<td>Status:</td>    
-				  				<td> 
-				  					<select name="status">
-									  	<?php status($status); ?>
-							    		<option value="0">utestående avvik finnes</option>
-							    		<option value="1">alle avvik lukket</option>
-					  				</select>
-
-					  				<input type="submit" name="submit" class="pull-right" >	
-				  				</td>
-				  			</tr>
-		  				</tbody>
-		  		</table>
-		  	</form>
-  		</div>
-	</div>
-
-	<script type="text/javascript">
-		var karakter = document.getElementsByClassName("karakter");
-		for (var i = karakter.length - 1; i >= 0; i--) {
-			karakter[i].addEventListener("change", sjekkForm);
-		}
-		function sjekkForm(){
-        		if (this.value ==5) {
-                    var tekstfelt = this.parentElement.nextElementSibling.firstChild;
-                    tekstfelt.value = "Ikke vurdert";
-					tekstfelt.disabled = true;
-        			return false;
-        		}
-        		if (this.value ==4) {
-                    var tekstfelt = this.parentElement.nextElementSibling.firstChild;
-                    tekstfelt.value = "Ikke aktuelt";
-					tekstfelt.disabled = true;
-        			return false;
-        		}	
-        	}	
-	</script>
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script type="text/javascript">
+var karakter = document.getElementsByClassName("karakter");
+for (var i = karakter.length - 1; i >= 0; i--) {
+    karakter[i].addEventListener("change", sjekkForm);
+}
+function sjekkForm(){
+    if (this.value ==5) {
+        var tekstfelt = this.parentElement.nextElementSibling.firstChild;
+        tekstfelt.value = "Ikke vurdert";
+        tekstfelt.disabled = true;
+        return false;
+    }
+    if (this.value ==4) {
+        var tekstfelt = this.parentElement.nextElementSibling.firstChild;
+        tekstfelt.value = "Ikke aktuelt";
+        tekstfelt.disabled = true;
+        return false;
+    }	
+}	
+</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
--- tilsynsrapport/oppdaterTilsynsrapport.php
+++ tilsynsrapport/oppdaterTilsynsrapport.php
@@ -10,4 +10,70 @@ function tilsynsrapport($tilsynid) {
 	lukk($db);
 	return mysqli_fetch_assoc($svar);
 }
+
+function tilsynsbesoektype($tilsynsbesoektype) {
+    if($tilsynsbesoektype == 0) {
+        echo "<option value='0'>Ordinært</option>";
+    } else if ($tilsynsbesoektype == 1) {
+        echo "<option value='1'>oppfølgings -tilsyn</option>";
+    }
+}
+
+function status($status) {
+    if ($status == 0) {
+        echo "<option value='0'>utestående avvik finnes</option>";
+    } else if ($status == 1) {
+        echo "<option value='1'>alle avvik lukket</option>";
+    }
+}
+
+function kravpunkter($tilsynid){
+    $db = kobleOpp();
+    if($tilsynid){
+        $sqlspørring = ("SELECT * FROM `Kravpunkter` WHERE `tilsynid` like '$tilsynid';");
+        
+    }
+    else{
+        $sqlspørring = ("SELECT DISTINCT ordingsverdi,kravpunktnavn_no
+        FROM Kravpunkter;");
+    }
+    
+    $svar = mysqli_query( $db, $sqlspørring );
+    lukk($db);
+    
+    $rad = mysqli_fetch_assoc($svar);
+    while ($rad) {
+        $ordingsverdi = $rad['ordingsverdi'];
+        $kravpunktnavn_no = $rad['kravpunktnavn_no'];
+        $karakter;
+        $tekst_no;
+        if($tilsynid){
+            $karakter = $rad['karakter'];
+            $tekst_no = $rad['tekst_no'];
+        }
+        
+        $temaOrdingsverdi = substr($ordingsverdi,0,1).'_'.substr($ordingsverdi,2,3);
+        echo"
+        <tr>
+        <td>$ordingsverdi:</td>
+        <td>$kravpunktnavn_no:</td>
+        <td>
+        <select class='karakter' name='karakter$temaOrdingsverdi'>
+        <option value='' selected disabled hidden>$karakter</option>
+        <option value='0'>0</option>
+        <option value='1'>1</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+        <option value='4'>4</option>
+        <option value='5'>5</option>
+        </select>
+        </td>
+        <td><input type='text' name='beskrivelse$temaOrdingsverdi' style='width: 100%;' value = '$tekst_no'></td>
+        </tr>
+        ";
+        $rad= mysqli_fetch_assoc($svar);
+        
+    }
+}
+
 ?>
