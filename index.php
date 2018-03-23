9e6bc5d46a574bd49767868c44679087211584d5
Patrick Pedersen <patrick@ossia.net>
2018-03-23 16:48:31 +0100

    Lagt til bootstrap på index.html

--- index.php
+++ index.php
@@ -14,21 +14,19 @@ require_once 'sok/sok.php';
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
+    
     <?php include_once 'header.php'; ?>
-    <div class="container">
+    <div class="container text-center">
         <?php 
         starAlertInnlogg();
         $side = 'Location: index.php';
         logginn($side);
         ?>
-        <main>    
-            <h1>Hvilken smiley har bedriften fått?</h1>
-            <h2>Velg hva du vil søke på</h2>
-            <?php sok(); ?>
-        </main>
+        <h1>Hvilken smiley har bedriften fått?</h1>
+        <h2>Velg hva du vil søke på</h2>
+        <?php sok(); ?>
     </div>
     <?php include_once 'footer.php'; ?>
-
 
     <script src="sok/sok.js"></script>
     <script src="bibloteker/jquery/jquery-3.3.1.js"></script>
