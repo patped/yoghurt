625812156e021a62bffcd76881adda66e7e3dee6
Patrick Pedersen <patrick@ossia.net>
2018-03-23 19:27:10 +0100

    Lagt til bootstrap på bedriftsiden

--- leggTilBedrift.php
+++ leggTilBedrift.php
@@ -1,8 +1,10 @@
 <?php
-session_start();	
+session_start();
+if(!$_SESSION['adminrett']) {
+	header("Location: /401.php");
+}
 include_once 'database.php';
 include_once 'logginn/logginn.php';
-$db = kobleOpp();
 ?>
 <!doctype html>
 <html lang="no">
@@ -15,77 +17,71 @@ $db = kobleOpp();
 <body>
 	
 	<?php
-	if(!$_SESSION['adminrett']) {
-		header("Location: /401.php");
-	}
 	include_once 'header-footer/header.php'; 
-	?>
-	<?php 
     starAlertInnlogg();
     $side = 'Location: /leggTilBedrift.php';
     logginn($side);
-    ?>
-    <main>
+	?>
 
-	
-	    <?php 
-   
-
-		echo <<<EOT
+	<div class="container text-center">
+		
 		<h1>Legg til ny bedrift</h1>
-
 		<h2>Fyll ut skjema</h2>
 
-
-	
 		<form method="POST" action="registrerBedrift.php">
-			<table>
-			
-	  			<tr>
-	    			<td>TilsynsobjektID:</td>
-	    			<td><input type="text" name="TilsynsobjektID" ></td>
-	  			</tr>
+			<div class="table-responsive">
+				<div class="col-xs-4 col-xs-offset-4">
+					<table class="table">
+						<thead>
+							<th></th>
+							<th></th>
+						</thead>
+						<tbody>
+							<tr>
+								<td>TilsynsobjektID:</td>
+								<td><input type="text" name="TilsynsobjektID" ></td>
+							</tr>
 
-	  			<tr>
-	    			<td>Organisasjonsnummer:</td>
-	   				<td><input type="text" name="Organisasjonsnummer" ></td>
-	  			</tr>
+							<tr>
+								<td>Organisasjonsnummer:</td>
+								<td><input type="text" name="Organisasjonsnummer" ></td>
+							</tr>
 
-	  			<tr>
-	    			<td>Navn:</td>
-	   				 <td><input type="text" name="Navn" ></td>
-	  			</tr>
+							<tr>
+								<td>Navn:</td>
+									<td><input type="text" name="Navn" ></td>
+							</tr>
 
-	  			<tr>
-	   				 <td>Adresselinje1:</td>
-	   				 <td><input type="text" name="Adresselinje1" ></td>
-	 			 </tr>
-	 			
-	 			 <tr>
-	    			<td>Adresselinje2:</td>
-	    			<td><input type="text" name="Adresselinje2" ></td>
-	  			</tr> 
+							<tr>
+								<td>Adresselinje1:</td>
+								<td><input type="text" name="Adresselinje1" ></td>
+							</tr>
+							
+							<tr>
+								<td>Adresselinje2:</td>
+								<td><input type="text" name="Adresselinje2" ></td>
+							</tr> 
 
-	 			<tr>
-	    			<td>Postnummer:</td>
-	    			<td><input type="text" name="Postnummer" ></td>
-	  			</tr> 
+							<tr>
+								<td>Postnummer:</td>
+								<td><input type="text" name="Postnummer" ></td>
+							</tr>
 
-				<td></td><td></td><td><input type="submit" name="send"></td>
-			 
-			</table>
+							<tr>
+								<td></td>
+								<td><input type="submit" name="send" class="pull-right"></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
 		</form>
+	</div>
 
-EOT;
-	?>
-</main>
+	<?php include_once 'header-footer/footer.php'; ?>
+	
 
-<?php include_once 'header-footer/footer.php'; ?>
-<script src="bibloteker/jquery/jquery-3.3.1.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<script src="bibloteker/jquery/jquery-3.3.1.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
-
 </html>
-<?php
-	lukk($db);
-?>
