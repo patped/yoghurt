0cdb81947aa6a3e2857d4206ff085093f6a26030
Kim <kimandreness@gmail.com>
2018-03-26 07:51:25 +0200

    Merge branch 'feature/ny.bruker.pa.admin.panelet' into develop

--- tilsynsrapport/tilsyn.php
+++ tilsynsrapport/tilsyn.php
@@ -15,8 +15,9 @@ require_once '../logginn/logginn.php';
 <body>
   <?php include_once '../div/header.php'; ?>
   <?php 
+    $til = $_GET['tilsynid'];
     starAlertInnlogg();
-    $side = 'Location: /tilsynsrapport/tilsyn.php?tilsynid=' . $_GET['tilsynid'] . '&dato=' . $_GET['dato'];
+    $side = 'Location: /tilsynsrapport/tilsyn.php?tilsynid=' . $til . '&dato=' . $_GET['dato'];
     logginn($side);
     ?>
 
@@ -27,8 +28,18 @@ require_once '../logginn/logginn.php';
   </div>
 
   <div class="container">
-    <div class="page-header"> <h2>Tilsynsrapport for dato: <?php echo $dato; ?> <a href="endre.php"><button type="button">Oppdater</button></a></h2> </div>
-    <div class="table-responsive">
+    <div class="page-header"> <h2>Tilsynsrapport for dato: <?php echo $dato ?></h2><br>
+      <?php
+      if(isset($_SESSION['adminrett']))
+        if(($_SESSION['adminrett'])){
+          echo <<< EOT
+          <h3>$til</h3><br>
+          <a href='endre.php'><button type='button'>Oppdater</button></a></h2>
+          <br>"
+EOT;
+  }
+      ?> </div>
+    <div class="table-responsive">"
       <?php tilsynsrapport(); ?>
     </div>
   </div>
