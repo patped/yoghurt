574e4f70c619c974fba0ba7b071e82ed6be7c94b
Kim <kimandreness@gmail.com>
2018-04-06 12:10:41 +0200

    Standardisert utseende på legg til bedrift og legg til ny bruker

--- admin/ny-bedrift.php
+++ admin/ny-bedrift.php
@@ -13,6 +13,7 @@ include_once '../logginn/logginn.php';
 	<title>Yoghurt</title>
   	<meta charset="utf-8">
   	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" type="text/css" href="fellesStilAdmin.css">
   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 <body>
