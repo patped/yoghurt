4333d22456b4cb2c2f22f1e660b78c2e67cf6d8c
Kim <kimandreness@gmail.com>
2018-04-15 20:14:43 +0200

    Ajax på orgnummer

--- sok/sokAlleOrg.php
+++ sok/sokAlleOrg.php
@@ -1,7 +1,7 @@
 <?php
 require_once '../div/database.php';
 $db = kobleOpp();
-$sql="SELECT orgnummer, navn from Restauranter";
+$sql="SELECT orgnummer, navn, tilsynsobjektid from Restauranter";
 $resultat = mysqli_query($db, $sql);
 $alleOrg = $resultat->fetch_all(MYSQLI_ASSOC);
 $inOrg = $_GET['innOrg'];
@@ -12,11 +12,13 @@ $inOrg = $_GET['innOrg'];
 				if($pos==0)
 					
 					echo "<li>";
-					echo "<a href= index.php>";
+					$side = '/restaurant.php?res=' . $org['tilsynsobjektid']; 
+					echo "<a href=" . $side;
+					echo ">";
 					echo $org['orgnummer'];
 					echo " : ";
 					echo $org['navn'];
-					echo "<a>";
+					echo "</a>";
 					echo "</li>";
 					
 		}
