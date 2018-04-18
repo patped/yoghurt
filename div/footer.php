6d0ca8c88c55226a1a3dbda1a57789100b14d4b4
Mathias <moxnes_1@hotmail.com>
2018-04-18 17:06:07 +0200

    endret feil fra id til class

--- div/footer.php
+++ div/footer.php
@@ -3,11 +3,11 @@
         <p id="paragraf" >Oppgave av Ola, Mathias, Patrick, Kim og Ingeborg.</p>
         <div id="divFotter">
             <a href="http://jigsaw.w3.org/css-validator/check/referer?profile=css3" target="_blank">
-                <img id="w3" src="/bilder/w3_css.png" alt="Valid CSS!" />
+                <img class="w3" src="/bilder/w3_css.png" alt="Valid CSS!" />
             </a>
             
             <a href="http://validator.w3.org/check?uri=referer" target="_blank">
-                <img id="w3" src="/bilder/W3C_HTML5_certified.png" alt="Valid HTML 5" />
+                <img class="w3" src="/bilder/W3C_HTML5_certified.png" alt="Valid HTML 5" />
             </a>
         </div>
     </footer>
