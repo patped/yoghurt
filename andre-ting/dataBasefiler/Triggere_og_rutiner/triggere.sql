d1b0809b40b10ff6874e37db17c1d4cccde0d3d4
Ola <olastaalberg@gmail.com>
2018-04-18 15:48:50 +0200

    Legg til ny bruker har nå forretningsregler
    Får en tabell med error om forretningsregler ikke følges

--- andre-ting/dataBasefiler/Triggere_og_rutiner/triggere.sql
+++ andre-ting/dataBasefiler/Triggere_og_rutiner/triggere.sql
@@ -5,3 +5,17 @@ SET NEW.startDato = NOW();
 CREATE TRIGGER Brukere_slutt_dato BEFORE INSERT ON Brukere
 FOR EACH ROW 
 SET NEW.sluttDato = NOW() + INTERVAL 30 DAY;
+
+DELIMITER $$
+DROP TRIGGER IF EXISTS brukernavnValidering $$
+CREATE TRIGGER brukernavnValidering BEFORE INSERT ON Brukere
+FOR EACH ROW
+BEGIN
+IF CHAR_LENGTH(NEW.brukernavn) < 5 THEN
+	SIGNAL SQLSTATE '10000' 
+    SET MESSAGE_TEXT = 'Brukernavn må inneholde minst 5 tegn';
+    ELSEIF NEW.brukernavn REGEXP '[0-9]' = 0 THEN
+    SIGNAL SQLSTATE '20000' 
+    SET MESSAGE_TEXT = 'Brukernavn må inneholde minst 1 tall';
+END IF;
+END $$
