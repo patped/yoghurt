645bc3cf009c58785ffc919b1e1bf23e050739ca
Mathias <moxnes_1@hotmail.com>
2018-04-18 18:26:35 +0200

    Merge konflikt fixxxxxx

--- admin/ny-bruker-landingside.php
+++ admin/ny-bruker-landingside.php
@@ -16,14 +16,6 @@ $db = kobleOpp();
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="/bibloteker/bootstrap-3.3.7-dist/css/bootstrap.min.css"> 
-    <style type="text/css">
-    	#nyBrukerError{
-    		text-align: center;
-    	}
-    	#tabellError{
-    		margin: auto;
-    	}
-    </style>
 </head>
 <body>
 	<?php
@@ -39,81 +31,85 @@ $db = kobleOpp();
 	$pass = htmlentities($_POST['passord']);
 	$passord = password_hash($pass, PASSWORD_DEFAULT);
 	$tlf = htmlentities($_POST['tlf']);
-	if(isset($_REQUEST['admi']))
+	if(isset($_REQUEST['admi'])) {
 		$admrett = 1;
-	else
+	} else {
 		$admrett = 0;
-
-			if (sjekkBrukerNavn($brukerNavn, $db)){
-				$sql=("INSERT INTO Brukere(brukernavn , Passord, telefonnummer, adminrettighet) VALUES (?,?,?,'$admrett');");
-				$stmt = mysqli_prepare($db, $sql);
-				$resultat = mysqli_query($db,$sql);
-				mysqli_stmt_bind_param($stmt, 'sss' , $brukerNavn, $passord, $tlf);
-				mysqli_stmt_execute($stmt);
-				$error = mysqli_stmt_error($stmt);
-				if (mysqli_errno($db)<> 0) {
-					$errno = mysqli_errno($db);
-					$sqlstate = mysqli_sqlstate($db);
-					$sqlerror = mysqli_error($db);
-					echo <<<EOT
-					<div class='jumbotron'>
-					<h1 class='text-center'>Feil ved brukernavn</h1>	
-					</div>
-					<div id='nyBrukerError'><table id='tabellError' class='table-striped'><tr><th>Feil oppstått</th><th>Feilkoden</th></tr>
-					<tr><td>$sqlerror</td><td>$sqlstate</td></tr>
-					<tr><td colspan='2'><button type='button' onclick="window.location.href='/admin/ny-bruker.php'">Prøv på nytt</td></tr></table></div>"
-EOT;
-
-				}
-
-
-
-				else if(!$error){
-				echo <<<EOT
-				<div class="jumbotron">
-			<h1 class="text-center">Suksess!</h1>	
-			</div>
-				<div class="container text-center">
-					<h1>Suksess</h1>
-					<h2>Du har registrert følgende bruker: </h2>
-					<table class='table table-hover'>
-
-						
-					
-			  			<tr>
-			    			<td>BrukerNavn:</td>
-			    			<td>$brukerNavn</td>
-			  			</tr>
-
-			  			<tr>
-			    			<td>Telefon</td>
-			   				<td>$tlf</td>
-			  			</tr>
-			 			
-
-					 
-					</table>
-					<a href="ny-bruker.php">Registrer en ny</a>	
-					</div>
-EOT;
-}
-else if($error){
-	echo <<<EOT
-	<div class="jumbotron">
-			<h1 class="text-center">Noe gikk galt</h1>	
-		</div>
-	<p>Ooops, her skjedde det noe feil og bruker er ikke registrert</p>
-	
-EOT;
-}
-}
-else {
-	echo "<h3>En bruker med samme brukernavn eksisterer <a href='ny-bruker.php'>Registrer</a> med ett annet Brukernavn</h3>";
 	}
+	if (sjekkBrukerNavn($brukerNavn, $db)){
+		$sql=("INSERT INTO Brukere(brukernavn , Passord, telefonnummer, adminrettighet) VALUES (?,?,?,'$admrett');");
+		$stmt = mysqli_prepare($db, $sql);
+		$resultat = mysqli_query($db,$sql);
+		mysqli_stmt_bind_param($stmt, 'sss' , $brukerNavn, $passord, $tlf);
+		mysqli_stmt_execute($stmt);
+		$error = mysqli_stmt_error($stmt);
+		if (mysqli_errno($db)<> 0) {
+			$errno = mysqli_errno($db);
+			$sqlstate = mysqli_sqlstate($db);
+			$sqlerror = mysqli_error($db);
+			echo <<<EOT
+			<div class='jumbotron'>
+				<h1 class='text-center'>Feil ved brukernavn</h1>	
+			</div>
+			
+			<div style='width: 33%; margin: auto'>
+				<table class='table table-striped'>
+					<tr>
+						<th>Feil oppstått</th><th>Feilkoden</th>
+					</tr>
+
+					<tr>
+						<td>$sqlerror</td><td>$sqlstate</td>
+					</tr>
+
+					<tr>
+						<td colspan='2'><button type='button' onclick="window.location.href='/admin/ny-bruker.php'">Prøv på nytt</button></td>
+					</tr>
+				</table>
+			</div>
+EOT;
+
+		}
 
 
 
-		
+		else if(!$error){
+			echo <<<EOT
+			<div class="jumbotron">
+				<h1 class="text-center">Suksess!</h1>	
+			</div>
+
+			<div class="container text-center">
+				<h2>Suksess</h2>
+				<h3>Du har registrert følgende bruker: </h3>
+				<table class='table table-hover'>
+					<tr>
+						<td>BrukerNavn:</td>
+						<td>$brukerNavn</td>
+					</tr>
+
+					<tr>
+						<td>Telefon</td>
+						<td>$tlf</td>
+					</tr>
+					
+				</table>
+				<a href="ny-bruker.php">Registrer en ny</a>	
+			</div>
+EOT;
+		} else if($error){
+			echo <<<EOT
+			<div class="jumbotron">
+				<h1 class="text-center">Noe gikk galt</h1>	
+			</div>
+			<p>Ooops, her skjedde det noe feil og bruker er ikke registrert</p>
+			
+EOT;
+		}
+	} else {
+		echo "<h3>En bruker med samme brukernavn eksisterer <a href='ny-bruker.php'>Registrer</a> med ett annet Brukernavn</h3>";
+	}
+	
 	include_once '../div/footer.php';	 ?>
 
     <script src="/bibloteker/jquery/jquery-3.3.1.min.js"></script>
